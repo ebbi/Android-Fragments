82b61313995fd7be97f084a0c1d3834440411b15
Ebbi <ebbi40@gmail.com>
2017-10-17 20:23:11 +0100

    recyclerview library added and test empty recyclerView display

--- app/src/main/java/com/example/todofragments/TodoListFragment.java
+++ app/src/main/java/com/example/todofragments/TodoListFragment.java
@@ -1,6 +1,12 @@
 package com.example.todofragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Ebbi on 17/10/2017.
@@ -8,4 +14,20 @@ import android.support.v4.app.Fragment;
 
 public class TodoListFragment extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_todo_list, container, false);
+
+        RecyclerView todoRecyclerView = (RecyclerView) view.findViewById(R.id.todo_recycler_view);
+        todoRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+
+    }
+
+
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
